639cfc24cae15066fe001b1dfc8378eee9087e9a
afridi <afridi.mahafuz@gmail.com>
2021-02-17 09:43:06 +0600

    java Loop II - HackerRank

--- HackerRankPractice/src/hackerrankpractice/HackerRankPractice.java
+++ HackerRankPractice/src/hackerrankpractice/HackerRankPractice.java
@@ -1,5 +1,6 @@
 package hackerrankpractice;
 import java.util.Scanner;
+import java.lang.Math;
 
 
 public class HackerRankPractice {
@@ -8,13 +9,30 @@ public class HackerRankPractice {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        System.out.println("================================");
-        for(int i=0; i<3; i++){
-            String s1 = sc.next();
-            int x = sc.nextInt();
-            System.out.printf("%-15s%03d%n", s1, x);
+        int query = sc.nextInt();
+        for(int i=0; i<query; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+            
+            int sum = 0;
+            
+            for(int j=0; j<n; j++){
+                if(j==0){
+                    sum += a + Math.pow(2, j) * b;
+                }
+                else{
+                    sum += Math.pow(2, j) * b;
+                } 
+                System.out.print(sum);
+                
+                if(j<n-1){
+                    System.out.print(" ");
+                }
+            }
+            
+            System.out.print("\n");
         }
-        System.out.println("================================");
     }
     
 }
